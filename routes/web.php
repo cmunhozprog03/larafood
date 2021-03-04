df39a6702301ca5f206e23322a51b5cdf15c2c9d
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-03-04 09:17:11 -0300

    wep.app update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+// Routes Plans
 Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plans.show');
 Route::post('admin/plans', 'Admin\PlanController@store')->name('plans.store');
 Route::get('admin/plans/create', 'Admin\PlanController@create')->name('plans.create');
